b8967e1700f9ed70f802667e0d8ed53ccb44455f
Erin Green <erin.y.green@gmail.com>
2018-09-10 20:32:15 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -44,10 +44,10 @@ def play(board)
   until counter >= 9
     turn(board)
     counter += 1
-=======
-  until counter > 9
-    counter += 1
-    turn(board)
+#
+  #until counter > 9
+  #  counter += 1
+  #  turn(board)
 
 end
 end 
